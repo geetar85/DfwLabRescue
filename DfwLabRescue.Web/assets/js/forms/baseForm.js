2d20dba6886b899859725d849ce8ed6fc92ff13a
geetar85 <brett.grosvenor@gmail.com>
2014-04-29 20:12:17 -0500

    added validation to adoption form

---
+++ DfwLabRescue.Web/assets/js/forms/baseForm.js
@@ -0,0 +1,174 @@
+var BaseForm = function () {
+
+    return {
+        
+        //Order Form
+        initBaseForm: function () {
+            // Masking
+            $('.phone').mask('(999) 999-9999', { placeholder: 'X' });
+            $('#year').mask('2099', { placeholder: 'X' });
+
+            $('.has-datepicker').datepicker({
+                dateFormat: 'dd.mm.yy',
+                prevText: '<i class="fa fa-angle-left"></i>',
+                nextText: '<i class="fa fa-angle-right"></i>'
+            });
+
+	        // Validation
+	        $("form").validate({
+	            // Rules for form validation
+	            // Rules for form validation
+	            rules:
+	            {
+	                fname:
+	                {
+	                    required: true
+	                },
+	                lname:
+	                {
+	                    required: true
+	                },
+	                email:
+	                {
+	                    required: true,
+	                    email: true
+	                },
+	                phone:
+	                {
+	                    required: true
+	                },
+	                country:
+	                {
+	                    required: true
+	                },
+	                city:
+	                {
+	                    required: true
+	                },
+	                zipcode:
+	                {
+	                    required: true,
+	                    digits: true
+	                },
+	                address:
+	                {
+	                    required: true
+	                },
+	                name:
+	                {
+	                    required: true
+	                },
+	                card:
+	                {
+	                    required: true,
+	                    creditcard: true
+	                },
+	                cvv:
+	                {
+	                    required: true,
+	                    digits: true
+	                },
+	                month:
+	                {
+	                    required: true
+	                },
+	                year:
+	                {
+	                    required: true,
+	                    digits: true
+	                }
+	            },
+	            // Messages for form validation
+	            messages:
+	            {
+	                fname:
+	                {
+	                    required: 'Please enter your first name'
+	                },
+	                lname:
+	                {
+	                    required: 'Please enter your last name'
+	                },
+	                email:
+	                {
+	                    required: 'Please enter your email address',
+	                    email: 'Please enter a VALID email address'
+	                },
+	                phone:
+	                {
+	                    required: 'Please enter your phone number'
+	                },
+	                country:
+	                {
+	                    required: 'Please select your country'
+	                },
+	                city:
+	                {
+	                    required: 'Please enter your city'
+	                },
+	                zipcode:
+	                {
+	                    required: 'Please enter code',
+	                    digits: 'Digits only please'
+	                },
+	                address:
+	                {
+	                    required: 'Please enter your full address'
+	                },
+	                name:
+	                {
+	                    required: 'Please enter name on your card'
+	                },
+	                card:
+	                {
+	                    required: 'Please enter your card number'
+	                },
+	                cvv:
+	                {
+	                    required: 'Enter CVV2',
+	                    digits: 'Digits only'
+	                },
+	                month:
+	                {
+	                    required: 'Select month'
+	                },
+	                year:
+	                {
+	                    required: 'Enter year',
+	                    digits: 'Digits only please'
+	                }
+	            },
+
+
+	            // Ajax form submition
+	            submitHandler: function(form)
+	            {
+	                $(form).ajaxSubmit(
+	                {
+	                    beforeSend: function()
+	                    {
+	                        $('form button[type="submit"]').addClass('button-uploading').attr('disabled', true);
+	                    },
+	                uploadProgress: function(event, position, total, percentComplete)
+	                {
+	                    $("form.progress").text(percentComplete + '%');
+	                },
+	                    success: function()
+	                    {
+	                        $("form").addClass('submited');
+	                        $('form button[type="submit"]').removeClass('button-uploading').attr('disabled', false);
+	                    }
+	                });
+	            },  
+	            
+	            // Do not change code below
+	            errorPlacement: function(error, element)
+	            {
+	                error.insertAfter(element.parent());
+	            }
+	        });
+        }
+
+    };
+
+}();
