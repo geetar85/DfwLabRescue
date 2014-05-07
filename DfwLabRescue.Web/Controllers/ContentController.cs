ee05d29c246a96d57e006f1f68eefdc2d1ae62d4
geetar85 <brett.grosvenor@gmail.com>
2014-05-06 20:01:52 -0500

    removed test exception throwing

--- DfwLabRescue.Web/Controllers/ContentController.cs
+++ DfwLabRescue.Web/Controllers/ContentController.cs
@@ -15,7 +15,6 @@ namespace DfwLabRescue.Web.Controllers
         [HttpGet]
         public ActionResult ContentPage(string contentId)
         {
-            throw new Exception();
             var content = DB.AllContent.FirstOrDefault(c => c.ContentId.ToLower() == contentId.ToLower());
             if (content == null)
                 return HttpNotFound();
